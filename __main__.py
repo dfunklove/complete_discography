231faefd714591a6f0f01892795fc6d7530ec543
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2020-07-30 21:49:44 -0500

    Works with new signature for get_discography

--- __main__.py
+++ __main__.py
@@ -4,7 +4,8 @@ from .complete_discography import get_discography
 # Execute the below command from outside complete_discography folder.
 
 if (len(sys.argv) < 2):
-	print("Usage: python -m complete_discography [artist name] > outfile.html")
+	print("Usage: python -m complete_discography [discogs artist id] > outfile.html")
+	print("(Must obtain artist id from discogs.com.  Look up an artist and the id will be in the url.)")
 	exit()
 
 print(get_discography(sys.argv[1]))
